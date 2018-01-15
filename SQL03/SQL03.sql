9b40013561c1d5478c4d68d5c5e2f891ecf61ac1
remys-note <remys-note@remys-note>
2018-01-15 14:03:44 +0900

    풀이

--- SQL03/SQL03.sql
+++ SQL03/SQL03.sql
@@ -82,6 +82,6 @@ order by e.last_name asc;
 */
 select e.employee_id, e.last_name, e.hire_date, m.last_name, m.hire_date
   from employees e, employees m
-  where e.manager_id = m.employee_id
-  and e.hire_date <  m.hire_date;
+ where e.manager_id = m.employee_id
+   and e.hire_date <  m.hire_date;
 
